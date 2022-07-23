bb1f46972a6f7e33922291663919e880cee8f1a1
James Park <87226158+JamesPark89@users.noreply.github.com>
2022-07-23 22:30:33 +1000

    Update Movie.js
    -Add Grid system

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,20 @@
+import * as React from "react"
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
+import { styled } from "@mui/material/styles"
+import Box from "@mui/material/Box"
+import Paper from "@mui/material/Paper"
+import Grid from "@mui/material/Grid"
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  margin: "1rem",
+  textAlign: "center",
+  color: theme.palette.text.secondary
+}))
 
 function Movie({ searchTerm }) {
   const [loading, setLoading] = useState(true)
@@ -25,28 +40,42 @@ function Movie({ searchTerm }) {
   }, [searchTerm])
 
   return (
-    <div>
-      <h1>movies</h1>
+    <Box>
+      <h1>Recent movies</h1>
       {loading ? (
         <h2> Loading...</h2>
       ) : noresult ? (
         <h2>No result</h2>
       ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <Link to={`/movie/${movie.id}`}>
-                <img
-                  src={movie.medium_cover_image}
-                  alt={movie.slug}
-                ></img>
-              </Link>
-              <li>{movie.title_long}</li>
-            </div>
-          ))}
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid
+            container
+            justifyContent="center"
+            spacing={{ xs: 2, md: 3 }}
+            column={{ xs: 4, sm: 8, md: 12 }}
+          >
+            {movies.map((movie) => (
+              <Item key={movie.id}>
+                <Link to={`/movie/${movie.id}`}>
+                  <img
+                    src={movie.medium_cover_image}
+                    alt={movie.slug}
+                  ></img>
+                </Link>
+                <li
+                  style={{
+                    listStyleType: "none",
+                    maxWidth: "230px"
+                  }}
+                >
+                  {movie.title_long}
+                </li>
+              </Item>
+            ))}
+          </Grid>
+        </Box>
       )}
-    </div>
+    </Box>
   )
 }
 
